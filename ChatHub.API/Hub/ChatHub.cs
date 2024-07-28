d641fa05b6d08824ff17fab41fbecd7471e47073
shust3r <yardonesenchuk@icloud.com>
2024-07-28 19:58:59 +0300

    feat(api): add chat endpoint

---
+++ ChatHub.API/Hub/ChatHub.cs
@@ -0,0 +1,53 @@
+﻿using ChatHub.API.Models;
+using Microsoft.AspNetCore.SignalR;
+
+namespace ChatHub.API.Hub;
+
+public class ChatHub : Microsoft.AspNetCore.SignalR.Hub
+{
+    private readonly IDictionary<string, UserRoomConnection> _connection;
+
+    public ChatHub(IDictionary<string, UserRoomConnection> connection)
+    {
+        _connection = connection;
+    }
+
+    public async Task JoinRoom(UserRoomConnection userConnection)
+    {
+        await Groups.AddToGroupAsync(Context.ConnectionId, userConnection.RoomName!);
+        _connection[Context.ConnectionId] = userConnection;
+        await Clients.Groups(userConnection.RoomName!)
+            .SendAsync("ReceiveMessage", "Bot", $"{userConnection.UserName} has joined the Group");
+        await SendConnectedUser(userConnection.RoomName!);
+    }
+
+    public async Task SendMessage(string message)
+    {
+        if(_connection.TryGetValue(Context.ConnectionId, out UserRoomConnection userRoomConnection))
+        {
+            await Clients.Group(userRoomConnection.RoomName!)
+                .SendAsync("ReceiveMessage", userRoomConnection.UserName, message, DateTime.UtcNow);
+        }
+    }
+
+    public override Task OnDisconnectedAsync(Exception? exception)
+    {
+        if (!_connection.TryGetValue(Context.ConnectionId, out UserRoomConnection connection))
+        {
+            return base.OnDisconnectedAsync(exception);
+        }
+
+        Clients.Group(connection.RoomName!)
+            .SendAsync("ReceiveMessage", "Bot", $"{connection.UserName} has left the Chat");
+        SendConnectedUser(connection.RoomName!);
+        return base.OnDisconnectedAsync(exception);
+    }
+
+    public Task SendConnectedUser(string room)
+    {
+        var users = _connection.Values
+            .Where(u => u.RoomName == room)
+            .Select(r => r.UserName);
+        return Clients.Group(room).SendAsync("ConnectedUser", users);
+    }
+}
---
+++ ChatHub.API/Models/UserRoomConnection.cs
@@ -0,0 +1,7 @@
+﻿namespace ChatHub.API.Models;
+
+public record UserRoomConnection
+{
+    public string? UserName { get; init; }
+    public string? RoomName {  get; init; }
+}
